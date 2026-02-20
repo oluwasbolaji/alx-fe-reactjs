781a0b86935732db10f9973aadbf27a7bc58431b
Oluwasola Bolaji <oluwasbolaji1993@gmail.com>
2026-02-20 17:53:04 +0100

    fix: Todolist to make it functional

---
+++ react-todo/src/__tests__/TodoList.test.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import TodoList from "./TodoList";
+
+describe("TodoList Component", () => {
+
+  test("renders initial todos", () => {
+    render(<TodoList />);
+    expect(screen.getByText("Learn React")).toBeInTheDocument();
+    expect(screen.getByText("Build a Todo App")).toBeInTheDocument();
+  });
+
+  test("adds a new todo", () => {
+    render(<TodoList />);
+
+    const input = screen.getByPlaceholderText("Add new todo");
+    const addButton = screen.getByText("Add");
+
+    fireEvent.change(input, { target: { value: "Write tests" } });
+    fireEvent.click(addButton);
+
+    expect(screen.getByText("Write tests")).toBeInTheDocument();
+  });
+
+  test("toggles a todo", () => {
+    render(<TodoList />);
+    const todo = screen.getByText("Learn React");
+
+    fireEvent.click(todo);
+
+    expect(todo).toHaveStyle("text-decoration: line-through");
+  });
+
+  test("deletes a todo", () => {
+    render(<TodoList />);
+    const deleteButton = screen.getAllByText("Delete")[0];
+
+    fireEvent.click(deleteButton);
+
+    expect(screen.queryByText("Learn React")).not.toBeInTheDocument();
+  });
+
+});
